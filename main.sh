67bc6449aa5499f011f8b5bfe419270006aa5885
ferrreo <harderthanfire@gmail.com>
2023-06-16 18:54:17 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,8 +1,3 @@
-# Add dependent repositories
-wget -q -O - https://ppa.pika-os.com/key.gpg | sudo apt-key add -
-add-apt-repository https://ppa.pika-os.com
-add-apt-repository ppa:pikaos/pika
-add-apt-repository ppa:kubuntu-ppa/backports
 # Clone Upstream
 git clone https://github.com/alsa-project/tinycompress -b v1.2.8
 cp -rvf ./debian ./tinycompress/
@@ -13,8 +8,6 @@ for i in ../patches/*.patch; do patch -Np1 -i $i ;done
 LOGNAME=root dh_make --createorig -y -l -p tinycompress_1.2.8
 
 # Get build deps
-ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
-DEBIAN_FRONTEND=noninteractive apt-get install -y tzdata
 apt-get build-dep ./ -y
 
 # Build package
